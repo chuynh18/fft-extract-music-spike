77f0f07bcf388ffaac0761c143fda5b5567d6565
Chris <lamcraft2003@yahoo.com>
2025-07-17 00:05:00 -0700

    stub out reading a file into an ArrayBuffer (really should have just looked at what I already did in midijs)

--- assets/js/fft.js
+++ assets/js/fft.js
@@ -1,5 +1,21 @@
 const fileInput = document.getElementById("audioInput");
 
 fileInput.addEventListener("change", () => {
-    console.log(fileInput.files[0]);
+    const file = fileInput.files[0];
+
+    let fileArrayBuffer; // holds contents of file as an array buffer
+    
+    readFile(file).then(
+        result => {
+            fileArrayBuffer = result;
+            console.log(fileArrayBuffer);
+        }
+    );
 });
+
+async function readFile(file) {
+    const fileArrayBuffer = await file.arrayBuffer().then(buffer => buffer);
+    return fileArrayBuffer;
+}
+
+const audioContext = new AudioContext();
