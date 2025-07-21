64ad101bf0a89845d5f9ecd7b660736b49eb06f7
Chris <lamcraft2003@yahoo.com>
2025-07-20 23:21:54 -0700

    not sure what to do, so I just wrote down at a high level in plain English what I think needs to be done

--- assets/js/fft.js
+++ assets/js/fft.js
@@ -1,3 +1,8 @@
+const audioContext = new OfflineAudioContext();
+const analyser = audioContext.createAnalyser();
+analyser.fftSize = 2048; // defaults to 2048, but putting it here in case I want to change it
+
+
 const fileInput = document.getElementById("audioInput");
 
 fileInput.addEventListener("change", () => {
@@ -9,6 +14,9 @@ fileInput.addEventListener("change", () => {
         result => {
             fileArrayBuffer = result;
             console.log(fileArrayBuffer);
+
+            const source = audioContext.createBufferSource();
+            source.buffer = fileArrayBuffer;
         }
     );
 });
@@ -18,6 +26,14 @@ async function readFile(file) {
     return fileArrayBuffer;
 }
 
-const audioContext = new OfflineAudioContext();
-const analyser = audioContext.createAnalyser();
-analyser.fftSize = 2048; // defaults to 2048, but putting it here in case I want to change it
+/*
+Not sure how to do most of these things but fundamentally what I need to do is the following:
+1. load arbitrary digital audio file
+2. load file into web audio api (offline audio context?)
+3. Downmix to mono
+4. Split the file into smaller segments for FFT analysis
+5. Do FFT analysis on each chunk to extract frequency data
+6. Reject overtones
+7. collate when each frequency begins and ends then convert those into notes
+8. convert that data into the object format that my digital piano uses to play music
+*/
